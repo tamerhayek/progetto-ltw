242959a9f98128970036457c03685b3a612f02c6
mariadianacalugaru <calugaru.1893272@studenti.uniroma1.it>
2022-04-28 18:15:56 +0200

    fix prova registrazione

--- provaregistrazione/registrazione.js
+++ provaregistrazione/registrazione.js
@@ -6,7 +6,7 @@ const password=document.getElementById("password");
 const passwordConferma=document.getElementById("passwordconferma");
 const form=document.getElementById("form");
 
-document.querySelector("button")
+/*document.querySelector("button")
 .addEventListener("click",(event)=>{
     event.preventDefault();
 
@@ -63,7 +63,7 @@ function validaInput(){
      }
 
 }
-
+*/ 
 function validEmail(email){
     return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
  }
@@ -84,4 +84,83 @@ function setError(input,message){
     messageEle.innerText=message;  
     parent.classList.add("error");
     parent.classList.remove("success");
+}
+
+function validaNome(){
+    if(nome.value.trim()===""){
+        setError(nome,"Questo campo non può essere vuoto!");
+        return false;
+    }else{
+        setSuccess(nome);
+        return true;
+    }
+}
+
+function validaCognome(){
+    if(cognome.value.trim()===""){
+        setError(cognome,"Questo campo non può essere vuoto!");
+        return false;
+    }else{
+        setSuccess(cognome);
+        return true;
+    }
+}
+
+function validaEmail(){
+    if(email.value.trim()===""){
+        setError(email,"Questo campo non può essere vuoto!");
+        return false;
+    }else{
+        if(!validEmail(email.value.trim())){
+            setError(email,"Email non valida");
+            return false;
+        }else{
+            setSuccess(email);
+            return true;
+        }
+    }
+}
+
+function validaUsername(){
+    if(username.value.trim()===""){
+        setError(username,"Questo campo non può essere vuoto!");
+        return false;
+    }else if(username.value.trim().length<8){
+        setError(username, "Lo username deve contenere almeno 8 caratteri!");
+        return false;
+    }
+    else{
+        setSuccess(username);
+        return true;
+    }
+}
+
+function validaPassword(){
+    if(password.value.trim()===""){
+        setError(password,"Questo campo non può essere vuoto!");
+        return false;
+    }else if(password.value.trim().length<8){
+        setError(password, "La password deve contenere almeno 8 caratteri!");
+        return false;
+    }else{
+        setSuccess(password);
+        return true;
+    }
+}
+
+function validaPasswordConf(){
+    if(passwordConferma.value.trim()===""){
+        setError(passwordConferma,"Questo campo non può essere vuoto!");
+        return false;
+    }else{
+         if(passwordConferma.value.trim()!==password.value.trim()){
+            setError(passwordConferma,"La password scelta e quella di conferma non coincidono!");
+            return false;
+         }
+         else{
+            setSuccess(passwordConferma);
+            return true;
+         }
+     }
+
 }
