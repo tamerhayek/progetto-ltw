94a5aed95d5f2e6a32c0db329827d5a551300cdd
Tamer <tamerhayek15@gmail.com>
2022-05-09 11:04:43 +0200

    quiz init

--- quiz/duello/index.php
+++ quiz/duello/index.php
@@ -13,7 +13,37 @@
 </head>
 
 <body>
-
+  <div class="container">
+    <div class="quit">
+      <a href="../../">Quit</a>
+    </div>
+    <div class="quiz">
+      <div class="quiz-domanda">
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla consequuntur fugiat corrupti ab, excepturi blanditiis deserunt dolores laborum optio totam magni, fugit molestias voluptas similique vero vitae earum, quibusdam eum.</h2>
+      </div>
+      <div class="quiz-risposte">
+        <div class="quiz-risposta">
+          <?php
+            echo "<button>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla consequuntur fugiat corrupti ab, excepturi blanditiis deserunt dolores laborum optio totam magni, fugit molestias voluptas similique vero vitae earum, quibusdam eum.</button>";
+          ?>
+        </div>
+        <div class="quiz-risposta">
+          <?php
+            echo "<button>Ciao</button>";
+          ?>
+        </div>
+        <div class="quiz-risposta">
+          <?php
+            echo "<button>Ciao</button>";
+          ?>
+        </div>
+        <div class="quiz-risposta">
+          <?php
+            echo "<button>Ciao</button>";
+          ?>
+        </div>
+      </div>
+    </div>
 
 </body>
 
