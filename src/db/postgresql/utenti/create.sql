0f2b9d0011d9aa24eb49c81e8c4f26dc422500d6
Tamer <tamerhayek15@gmail.com>
2022-05-31 20:09:42 +0200

    Update create.sql

--- src/db/postgresql/utenti/create.sql
+++ src/db/postgresql/utenti/create.sql
@@ -4,7 +4,5 @@ create table utenti(
     email varchar(50),
     username varchar(50) not null primary key,
     password varchar(50) not null,
-    punteggio integer default 0,
-    partiteGiocate integer default 0,
     admin boolean default false
 );
